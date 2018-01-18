ea5322954bf73fb593b2bc8b48154d467d426433
Marek Dlugos <marek.dlugos@gmail.com>
2018-01-18 21:40:19 +0100

    🚗 Autos und Fraun machen uns glücklich

--- src/main/java/report/ConsumptionReport.java
+++ src/main/java/report/ConsumptionReport.java
@@ -6,6 +6,7 @@ import iterator.HouseIterator;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -13,56 +14,49 @@ import java.util.ArrayList;
 public class ConsumptionReport implements Report {
   private List<Device> devices;
 
-  private static double electricityCost = 3.5;
+  private static double electricityCost = 30.5;
   private static double waterCost = 2.5;
   private static double gasCost = 4.5;
+  private double totalElectricityCost = 0;
+  private double totalWaterCost = 0;
+  private double totalGasCost = 0;
+  private double totalElectricityConsumption = 0;
+
+  DecimalFormat df = new DecimalFormat("#.##");
 
   public ConsumptionReport(ArrayList<Device> devices) {
     this.devices = devices;
   }
 
-  private Integer totalConsumption(Device device) {
-    return 0;
-  }
-
   public void print() {
-//    PrintWriter writer = null;
-//    try {
-//      writer = new PrintWriter("/Users/Marek/Desktop/Consumption-Report.txt", "UTF-8");
-//      writer.println("------------------------- CONSUMPTION REPORT -------------------------");
-//      writer.println(" ");
-//      for(Device device : devices) {
-//        writer.println(device.getClass());
-//      }
-//    } catch (FileNotFoundException e) {
-//      e.printStackTrace();
-//    } catch (UnsupportedEncodingException e) {
-//      e.printStackTrace();
-//    }
-//
-//    writer.close();
+    PrintWriter writer = null;
+    try {
+      writer = new PrintWriter("/Users/Marek/Desktop/Consumption-Report.txt", "UTF-8");
+      writer.println("------------------------- CONSUMPTION REPORT -------------------------");
+      writer.println(" ");
+      for(Device device : devices) {
+        double consumption = device.getEnergyConsumed();
+        double cost = device.getEnergyConsumed()*electricityCost;
 
-//    for(Device device : devices) {
-//      // time period
-//      // if(device.type == "ELECTRIC")
-//      // device.consumption =+ electricityCost;
-//
-//      // TODO: OUTPUT
-//      //
-//      // Consumption Report for last 24h (24 cycles):
-//      //
-//      //   Microwave
-//      //     In the idle state: 5 hour(s). [Idle state consumption per hour: 0.3][Consumption: xy kW][Cost: xy €]
-//      //     Turned on: 1 hour(s). [Turned on consumption per hour: 1.9][Consumption: xy kW][Cost: xy €]
-//      //     Turned off: 0 hour(s). [Turned off consumption per hour: 0.01][Consumption: xy kW][Cost: xy €]
-//      //
-//      //     Total Consumption: xy kW.
-//      //     Cost: xy €.
-//
-//      System.out.println("---------- CONSUMPTION REPORT FOR LAST 24H ----------");
-//      System.out.println("  " + device.getClass().getSimpleName() );
-////      System.out.println("      " + "In the idle state: " + idleState.hours + " hour(s)." + "[Idle state consumption per hour: " + idleState.Consumption + "]" + "[Consumption: " + (idleState.hours*idleState.Consumption) + " kW]" + "[Cost: " + (idleState.hours*idleState.Consumption)*electricityCost + " €]");
-//
-//    }
+        totalElectricityConsumption += consumption;
+        totalElectricityCost += cost;
+
+        writer.println("  " + device.getClass().getSimpleName());
+        writer.println("    " + "Consumed: " + df.format(consumption) + "kWh");
+        writer.println("    " + "Cost: " + df.format(cost) + "kWh");
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+
+    writer.println(" ");
+    writer.println("Total house electricity consumption: " + df.format(totalElectricityConsumption) + "kWh");
+    writer.println("Total house electricity cost: " + df.format(totalElectricityCost) + "€");
+    writer.println(" ");
+    writer.println("---------------------- END OF CONSUMPTION REPORT ----------------------");
+
+    writer.close();
   }
 }
